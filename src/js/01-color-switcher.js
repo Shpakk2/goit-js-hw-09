bbb8773b645af27fcb45ffa252c9c51a34531b2e
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-11-08 17:55:10 +0200

    1st-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const bodyEl = startBtn.parentNode
 
+stopBtn.disabled = true
+
+startBtn.addEventListener('click', startStyles)
+stopBtn.addEventListener("click", stopStyles)
+
+function startStyles() {
+    startBtn.disabled = true
+    stopBtn.disabled = false
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = `${getRandomHexColor()}`
+  }, 1000);
+}
+
+function stopStyles() {
+    startBtn.disabled = false
+        stopBtn.disabled = true
+    clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
